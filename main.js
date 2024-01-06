79c6d8112e59200d40bd22738e6e0af2b8d6919b
Sai Teja <saitejareddynov30@gmail.com>
2024-01-06 11:33:39 +0530

    wes soln

---
+++ main.js
@@ -0,0 +1,22 @@
+const checkboxes = document.querySelectorAll(".item input[type=checkbox]");
+
+let lastChecked;
+let inBetween = false;
+
+function handleCheck(e) {
+  if (e.shiftKey && this.checked) {
+    checkboxes.forEach((checkbox) => {
+      if ((checkbox === this || checkbox === lastChecked) && lastChecked) {
+        inBetween = !inBetween;
+      }
+      if (inBetween) {
+        checkbox.checked = true;
+      }
+    });
+  }
+  lastChecked = this;
+}
+
+checkboxes.forEach((checkbox) =>
+  checkbox.addEventListener("click", handleCheck)
+);
